7c08910fcddaac811cd2ec81f24e8da67c48d34f
Emil Tveden Bjerglund <emilbp@gmail.com>
2022-04-23 13:05:24 +0200

    Added skeleton for applicaton of default layout

--- src/preprocessor.ts
+++ src/preprocessor.ts
@@ -2,9 +2,9 @@ import * as Plotly from 'plotly.js-dist-min';
 
 import { MarkdownPostProcessorContext, parseYaml } from "obsidian";
 
-export const preprocessor = (content: string, el: HTMLElement, ctx: MarkdownPostProcessorContext)=>{
+export const preprocessor = (content: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
     let json;
-    try{
+    try {
         json = parseYaml(content);
         validate(json, el)
         render(json, el)
@@ -18,12 +18,12 @@ export const preprocessor = (content: string, el: HTMLElement, ctx: MarkdownPost
 const allowValues = ["data", "layout", "config"];
 
 const validate = (json: any, el: HTMLElement) => {
-    if(!json){
+    if (!json) {
         throw "There should be a valid JSON in this block."
     }
 
-    Object.keys(json).forEach(key=>{
-        if(!allowValues.contains(key)){
+    Object.keys(json).forEach(key => {
+        if (!allowValues.contains(key)) {
             throw "The only valid keys are data, layout and config."
         }
     })
@@ -33,14 +33,64 @@ const render = (json: any, el: HTMLElement) => {
     renderPlotly(el, json.data, json.layout, json.config)
 }
 
+
+function getWidth(): number {
+    const collection: HTMLCollection = document.getElementsByClassName("markdown-preview-section");
+    const firstElement = collection[0] as HTMLElement;
+    var width = firstElement.offsetWidth;
+    return width;
+}
+
+function getDefaultLayout(width: number): Partial<Plotly.Layout> {
+    return {
+    plot_bgcolor: "rgba(0,0,0,0)",
+    paper_bgcolor: "rgba(0,0,0,0)",
+    autosize: false,
+    width: width,
+    height: width * 0.5,
+    margin: {
+        l: 20,
+        r: 5,
+        b: 20,
+        t: 5,
+        pad: 0
+    },
+    xaxis: {
+        fixedrange: true,
+        showgrid: true,
+        zeroline: true,
+        showline: true,
+        gridcolor: '#ffffff',
+        zerolinecolor: '#ffffff',
+        linecolor: '#ffffff',
+        tickfont: { color: '#ffffff' }
+    },
+    yaxis: {
+        fixedrange: true,
+        showgrid: true,
+        zeroline: true,
+        showline: true,
+        gridcolor: '#ffffff',
+        zerolinecolor: '#ffffff',
+        linecolor: '#ffffff',
+        tickfont: { color: '#ffffff' }
+    }
+}
+};
+
+
 export const renderPlotly = (el: HTMLElement, data: Object[], layout: Object, config: Object) => {
     const destination = document.createElement('div');
 
-    if(el.firstElementChild!=null){
-        Plotly.update(destination, data as any, layout, config as any);        
+    let defaultLayout = getDefaultLayout(getWidth());
+
+    let mergedLayout = {...defaultLayout, ...layout };
+
+    if (el.firstElementChild != null) {
+        Plotly.update(destination, data as any, mergedLayout as any, config as any);
         el.replaceChild(destination, el.firstElementChild);
     } else {
-        Plotly.newPlot(destination, data, layout, config);        
+        Plotly.newPlot(destination, data, mergedLayout, config);
         el.appendChild(destination);
     }
 }
