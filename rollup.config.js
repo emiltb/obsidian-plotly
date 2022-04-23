d3a55ac4790f59e95d8fe6405e2da4b2201b721c
Emil Tveden Bjerglund <emilbp@gmail.com>
2022-04-23 13:05:06 +0200

    Change build location for easier workflow

--- rollup.config.js
+++ rollup.config.js
@@ -16,7 +16,7 @@ if you want to view the source visit the plugins github repository
 export default {
   input: 'src/main.ts',
   output: {
-    dir: './build/',
+    dir: './',
     sourcemap: 'inline',
     sourcemapExcludeSources: isProd,
     format: 'cjs',
